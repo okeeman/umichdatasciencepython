de9b1934b8ce1ebca40c6c1b046c22fd47d1d12a
okeeman <owen.sweeney@outlook.com>
2026-07-24 20:04:03 +0100

    Update logdata.py

--- logdata.py
+++ logdata.py
@@ -3,13 +3,17 @@ import re
 def logs():
     with open("logdata.txt", "r") as file:
         logdata = file.read()
-        
+
+    # Match between 1 to 3 digits, then a dot, etc.
     urls = re.findall(r'(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})', logdata)
 
+    # Match a hyphen, then a space, grab everything until match a space, then an opening square bracket.
     user_names = re.findall(r'-\s(.*?)\s\[', logdata)
 
+    # Match an opening square bracket, grb everything until match a closing square bracket.
     times = re.findall(r'\[(.*?)]', logdata)
 
+    # Grab everything in between quotation marks.
     requests = re.findall(r'\"(.*?)\"', logdata)
 
     log_data = []
